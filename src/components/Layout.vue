e01b273ce3df87a3078f53df236e3ae305296918
aod <1677120510@qq.com>
2021-07-28 21:44:43 +0800

    添加404

---
+++ src/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 全局组件 -->
+     <div class="nav-wrapper">
+        <div class="content"> 
+            <!--slot插槽，layout获取到其他页面的数据 -->
+            <slot/> 
+        </div>
+        <Nav/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .nav-wrapper{
+        border:1px solid  red;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        overflow: auto;
+        flex-grow: 1; //尽量把高度都给content
+    }
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        页面不存在，请检查路径是否正确~~~
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
