ccdbb7b3f9f3cc873b015826fe67d7f50343fb3e
aod <1677120510@qq.com>
2021-08-04 07:05:26 +0800

    用model.ts改写

--- src/costom.d.ts
+++ src/costom.d.ts
@@ -1,4 +1,4 @@
-//全局变量
+//全局类型，xxx.d.ts。这个可以直接使用，不需要再引入
 //声明一个记录，用ts必须先声明类型
 type RecordItem={
   tags:string[],
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,9 @@
 
 <template>
     <Layout class-prefix="layout">
-        {{recordList}}
-        <NumberPad @submit = "saveRecord" @update:value = "onUpdateAmount"/>
-        <Types :value.sync="record.type" />  <!--用.sync如果有初始值就用初始值，没有就导入外部数据-->
+        {{RecordItem}}
+        <NumberPad @submit = "saveRecordItem" @update:value = "onUpdateAmount"/>
+        <Types :value.sync="RecordItem.type" />  <!--用.sync如果有初始值就用初始值，没有就导入外部数据-->
         <Notes @update:value = "onUpdateNotes"/>
         <!-- 用sync把外部的文件导入到内部数据中 -->
         <Tags @update:value ="onUpdateTags" :dataSource.sync="tags" />   
@@ -17,44 +17,44 @@ import Types from '@/components/money/Types.vue';
 import Notes from '@/components/money/Notes.vue';
 import Tags from '@/components/money/Tags.vue';
 import {Component,Watch} from 'vue-property-decorator';
-const model = require('@/models/model').model //ts引入js时用require
+import model from '@/models/model';
 
 //声明一个记录，用ts必须先声明类型
-type Record={
+type RecordItem={
     tags:string[],
     notes:string,
     type:string,
     amount:number
     createdAt?:Date
 }
-const recordList: Record[] = model.fetch()
+const RecordItemList = model.fetch()
   @Component({
        components :{ NumberPad, Types, Tags, Notes}
   })
         export default class Money extends Vue{
          tags =['衣','食','住','行'];
-        record:Record={
+        RecordItem:RecordItem={
             tags:[],notes:'',type:'-', amount:0     //给个初始值  
             }
-        recordList: Record[] = recordList;
+        RecordItemList: RecordItem[] = RecordItemList;
          onUpdateTags(value:string []){
-             this.record.tags = value
+             this.RecordItem.tags = value
          }
          onUpdateNotes(value:string){
-             this.record.notes = value
+             this.RecordItem.notes = value
          }
          onUpdateAmount(value:string){
-             this.record.amount = parseFloat(value)
+             this.RecordItem.amount = parseFloat(value)
             
          } 
-         saveRecord() {
-      const record2: Record = JSON.parse(JSON.stringify(this.record));
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+         saveRecordItem() {
+      const RecordItem2: RecordItem = model.clone((this.RecordItem));
+      RecordItem2.createdAt = new Date();
+      this.RecordItemList.push(RecordItem2);
     }
-    @Watch('recordList')
-    onrecordListChange() {
-      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    @Watch('RecordItemList')
+    onRecordItemListChange() {
+      model.save((this.RecordItemList));
     }
         }
 </script>
@@ -69,5 +69,6 @@ const recordList: Record[] = model.fetch()
 <style lang="scss" scoped>
     @import "~@/assets/style/helper.scss";
     @import "~@/assets/style/reset.scss";
+
 
 </style>
